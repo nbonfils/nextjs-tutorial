306485b6b094cbf12a034b4652f82224b08c64d9
nbonfils <nils.bonfils@epfl.ch>
2020-05-20 20:53:59 +0200

    Add Goooogle Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/global.css'
 
+import Router from 'next/router';
+
+import * as gtag from '../lib/gtag';
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
